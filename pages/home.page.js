f479a3207ef3f2c44290ed61a6a5d72eb58a90fb
dhodi-fahad <dhodifahad@gmail.com>
2024-02-03 13:31:50 +0300

    HomePage Object functions

--- pages/home.page.js
+++ pages/home.page.js
@@ -1,14 +1,19 @@
+import {$} from "@wdio/globals";
 import Page from "./page.js";
 
 
 class HomePage extends Page {
-    get welcomeTitle(){
-        return $ ('h1[class="display-3"]')
-    }
-
     open() {
         return super.open("");
     }
+
+    get welcomeTitle(){
+        return $ ('h1[class="display-3"]')
+    }
+    get welcomeSubtitle(){
+        return $ ('p[class="lead"]')
+    }
+
 }
 
 export default new HomePage();
